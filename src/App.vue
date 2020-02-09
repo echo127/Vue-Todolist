ec8e4d5d40cb5eb33e8eacd43422e3de40dbdf10
Echo127 <echo127@gmail.com>
2020-02-09 20:45:58 +1300

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,44 @@
 <template>
   <div id="app">
-    <h1>Hello new job!</h1>
-    <Todos v-bind:todos="todos" />
+    <Header />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import Todos from "./components/Todos";
+import Header from './components/layout/header';
+import Todos from './components/Todos';
 export default {
-  name: "app",
+  name: 'app',
   components: {
-    Todos
+    Todos,
+    Header
   },
   data() {
     return {
       todos: [
         {
           id: 1,
-          title: "Todo One",
+          title: 'Todo One',
           completed: false
         },
         {
           id: 2,
-          title: "Todo Two",
+          title: 'Todo Two',
           completed: false
         },
         {
           id: 3,
-          title: "Todo Three",
+          title: 'Todo Three',
           completed: false
         }
       ]
     };
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    }
   }
 };
 </script>
